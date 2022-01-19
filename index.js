55aa69e92fc1e34851f38381e86e1872963170ad
CameronRuch <cameron.ruch@yahoo.com>
2022-01-19 12:55:08 -0600

    finished task 1 with Brit

--- index.js
+++ index.js
@@ -39,16 +39,46 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-  
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
+}
+Person.prototype.eat = function(edible){
+  if(this.stomach.length < 10){
+    this.stomach.push(edible);
+  }
+}
+Person.prototype.poop = function(){
+  this.stomach = [];
+}
+Person.prototype.toString = function(){
+  return `${this.name}, ${this.age}`;
 }
 
 
+// create new people
 
+const sallie = new Person('Sallie', 26);
+const mark = new Person('Mark', 23);
+const cori = new Person('Cori', 25);
 
+console.log(sallie.toString());
+console.log(mark.toString());
+console.log(cori.toString());
 
+mark.eat('Pizza');
+mark.eat('Pasta');
+mark.eat('Tacos');
+mark.eat('Sushi');
+mark.eat('Sandwich');
+mark.eat('Cake');
+mark.eat('Ramen');
 
+console.log(mark.stomach);
 
+mark.poop();
+console.log(mark.stomach);
 /*
   TASK 2
     - Write a Car constructor that initializes `model` and `milesPerGallon` from arguments.
